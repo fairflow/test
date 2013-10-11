7698437e0604bce7953712044de0d43bc7f6e443
Dr Matthew Fairtlough <m.fairtlough@open.ac.uk>
2013-10-11 11:32:37 +0000

    Space modifications only and they weren't necessary

--- src/Dispatch.hs
+++ src/Dispatch.hs
@@ -1,17 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
 
 -- This template is described in detail in the School of Haskell tutorials at
 -- https://www.fpcomplete.com/school/ide-tutorials/buiding-a-file-hosting-service-in-yesod
 
 module Dispatch where
 
-import Yesod
+import           Yesod
 
-import Foundation
-import Handler.Download
-import Handler.Home
-import Handler.Preview
+import   Foundation
+import           Handler.Download
+import           Handler.Home
+import           Handler.Preview
 
 mkYesodDispatch "App" resourcesApp
